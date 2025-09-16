420460e49a892c769342a25f9b6ee0f08588c379
Artur Havliukovskyi <agavlyukovskiy@gmail.com>
2025-09-16 12:57:23 +0200

    Added skeleton for repositories

---
+++ src/main/java/net/hackyourfuture/coursehub/repository/CourseRepository.java
@@ -0,0 +1,24 @@
+package net.hackyourfuture.coursehub.repository;
+
+import net.hackyourfuture.coursehub.data.CourseEntity;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CourseRepository {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public CourseRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<CourseEntity> findAll() {
+        String sql = "SELECT * FROM courses";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            CourseEntity course = new CourseEntity();
+            return course;
+        });
+    }
+}
--- src/main/java/net/hackyourfuture/coursehub/web/CourseController.java
+++ src/main/java/net/hackyourfuture/coursehub/web/CourseController.java
@@ -1,6 +1,8 @@
 package net.hackyourfuture.coursehub.web;
 
+import net.hackyourfuture.coursehub.repository.CourseRepository;
 import net.hackyourfuture.coursehub.web.model.CourseDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,17 @@ import java.util.List;
 @RestController
 public class CourseController {
 
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
     @GetMapping("/courses")
     public List<CourseDto> getAllCourses() {
-        return List.of();
+        return courseRepository.findAll()
+                .stream()
+                .map(c -> new CourseDto())
+                .toList();
     }
 }
