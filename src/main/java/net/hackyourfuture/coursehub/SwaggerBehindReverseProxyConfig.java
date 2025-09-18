278c75f05590508941684a00d2d5fb0c93b5ed9d
Artur Havliukovskyi <agavlyukovskiy@gmail.com>
2025-09-19 00:07:46 +0200

    Revert adjustment since digital ocean doesn't set any variables by default

--- src/main/java/net/hackyourfuture/coursehub/SwaggerBehindReverseProxyConfig.java
+++ src/main/java/net/hackyourfuture/coursehub/SwaggerBehindReverseProxyConfig.java
@@ -1,10 +1,6 @@
 package net.hackyourfuture.coursehub;
 
 import org.springdoc.core.customizers.ServerBaseUrlCustomizer;
-import org.springdoc.core.properties.SpringDocConfigProperties;
-import org.springdoc.core.properties.SwaggerUiConfigProperties;
-import org.springdoc.core.providers.SpringWebProvider;
-import org.springdoc.webmvc.ui.SwaggerWelcomeWebMvc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,26 +11,12 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerBehindReverseProxyConfig {
 
     @Bean
-    SwaggerWelcomeWebMvc swaggerWelcome(SwaggerUiConfigProperties swaggerUiConfig, SpringDocConfigProperties springDocConfigProperties, SpringWebProvider springWebProvider) {
-        if (isBehindDigitalOceanReverseProxy()) {
-            swaggerUiConfig.setUrl("/api/v3/api-docs");
-            swaggerUiConfig.setConfigUrl("/api/v3/api-docs/swagger-config");
-        }
-        return new SwaggerWelcomeWebMvc(swaggerUiConfig, springDocConfigProperties, springWebProvider);
-    }
-
-    @Bean
     ServerBaseUrlCustomizer serverBaseUrlCustomizer() {
         return (serverBaseUrl, request) -> {
-            if (isBehindDigitalOceanReverseProxy()) {
+            if (serverBaseUrl.contains("coursehub.hyf.dev")) {
                 return serverBaseUrl + "/api";
             }
             return serverBaseUrl;
         };
-    }
-
-    private boolean isBehindDigitalOceanReverseProxy() {
-        var appDomain = System.getenv("APP_DOMAIN");
-        return appDomain != null && appDomain.contains("coursehub.hyf.dev");
     }
 }
